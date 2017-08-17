790636c779d43f9858daafcc1613c3c39962391a
cash <cash@cashwu.com>
2017-08-17 16:43:25 +0800

    input 1 test case

--- Kata20170817_GiveMeADiamond/GiveMeADiamondTests.cs
+++ Kata20170817_GiveMeADiamond/GiveMeADiamondTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170817_GiveMeADiamond
     public class GiveMeADiamondTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_1_diamond()
         {
+            DiamondPrintShouldBe("*\n", 1);
+        }
+
+        private static void DiamondPrintShouldBe(string expected, int count)
+        {
+            var diamond = new Diamond();
+            var actual = diamond.print(count);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Diamond
+    {
+        public string print(int n)
+        {
+            return "*\n";
         }
     }
 }
